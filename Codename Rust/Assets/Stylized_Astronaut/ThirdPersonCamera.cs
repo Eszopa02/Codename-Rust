916bf5812534da3cfc9fbebb1c91a1352dd828ec
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2023-05-08 10:07:40 -0500

    Rotation Smooth Velocity

--- Codename Rust/Assets/Stylized_Astronaut/ThirdPersonCamera.cs
+++ Codename Rust/Assets/Stylized_Astronaut/ThirdPersonCamera.cs
@@ -9,8 +9,12 @@ public class ThirdPersonCamera : MonoBehaviour
 {
     public float mouseSensitivity = 10;
     public Transform target;
-    public float dstFromTarget = 2;
-    public Vector2 pitchMinMax = new Vector2(-45, 45);
+    public float dstFromTarget = 15;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = 0.12f;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
 
     float yaw;
     float pitch;
@@ -26,6 +30,9 @@ public class ThirdPersonCamera : MonoBehaviour
         yaw += Input.GetAxis ("Mouse X") * mouseSensitivity;
         pitch -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
         pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
 
         Vector3 targetRotation = new Vector3(pitch, yaw);
         transform.eulerAngles = targetRotation;
